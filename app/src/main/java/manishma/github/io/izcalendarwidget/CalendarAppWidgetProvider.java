052ba184f7fb2904e8124aa0596c32f37d5d2ae8
Igor Zelmanovich <igor.israel@gmail.com>
2015-01-15 16:09:22 +0200

    show calendar events

--- app/src/main/java/manishma/github/io/izcalendarwidget/CalendarAppWidgetProvider.java
+++ app/src/main/java/manishma/github/io/izcalendarwidget/CalendarAppWidgetProvider.java
@@ -3,46 +3,117 @@ package manishma.github.io.izcalendarwidget;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.provider.CalendarContract;
+import android.provider.CalendarContract.Events;
+import android.provider.CalendarContract.Instances;
+import android.text.format.Time;
 import android.widget.RemoteViews;
 
 import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.Locale;
+import java.util.List;
 
 public class CalendarAppWidgetProvider extends AppWidgetProvider {
+
+    public static final String[] INSTANCE_PROJECTION = new String[]{
+            Instances._ID,
+            Instances.TITLE,
+            Instances.BEGIN,
+            Instances.END,
+            Instances.ALL_DAY,
+            Instances.START_DAY,
+            Instances.END_DAY,
+            Instances.DISPLAY_COLOR,
+    };
+
+    // The indices for the projection array above.
+    private static final int INSTANCE_PROJECTION_ID_INDEX = 0;
+    private static final int INSTANCE_PROJECTION_TITLE_INDEX = 1;
+    private static final int INSTANCE_PROJECTION_BEGIN_INDEX = 2;
+    private static final int INSTANCE_PROJECTION_END_INDEX = 3;
+    private static final int INSTANCE_PROJECTION_ALL_DAY_INDEX = 4;
+    private static final int INSTANCE_PROJECTION_START_DAY_INDEX = 5;
+    private static final int INSTANCE_PROJECTION_END_DAY_INDEX = 6;
+    private static final int INSTANCE_PROJECTION_DISPLAY_COLOR_INDEX = 7;
+
+    final class EventData {
+        public String Title;
+        public long Start;
+        public long End;
+        public int Color;
+        public boolean AllDay;
+        public long StartDay;
+        public long EndDay;
+    }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 
         for (int i = 0; i < appWidgetIds.length; i++) {
 
-            RemoteViews widgetView = new RemoteViews(context.getPackageName(), R.layout.calendar_appwidget);
-            RemoteViews cellViewMaster = new RemoteViews(context.getPackageName(), R.layout.calendar_cell);
-
             Calendar cal = Calendar.getInstance();
             cal.setTime(new Date());
             int today = cal.get(Calendar.DAY_OF_WEEK);
             // recet to first day of the week
-            cal.setTimeInMillis(cal.getTimeInMillis() - cal.get(Calendar.DAY_OF_WEEK) * 24 * 60 * 60 * 1000);
-            DateFormat format = DateFormat.getDateInstance();
+            cal.add(Calendar.DAY_OF_WEEK, 1 - cal.get(Calendar.DAY_OF_WEEK));
+            cal.set(Calendar.HOUR_OF_DAY, 0);
+            cal.set(Calendar.MINUTE, 0);
+            cal.set(Calendar.SECOND, 0);
+            cal.set(Calendar.MILLISECOND, 0);
+
+            DateFormat dateFormat = DateFormat.getDateInstance();
+            DateFormat timeFormat = DateFormat.getTimeInstance(DateFormat.SHORT);
+
+            long fromMillis = cal.getTimeInMillis();
+            long toMillis = fromMillis + 7 * 24 * 3600000;
+            List<EventData> events = new ArrayList<>();
+            ContentResolver cr = context.getContentResolver();
+
+            // Construct the query with the desired date range.
+            Uri.Builder builder = Instances.CONTENT_URI.buildUpon();
+            ContentUris.appendId(builder, fromMillis);
+            ContentUris.appendId(builder, toMillis);
+            String selection = Instances.VISIBLE + "=1";
+            Cursor cur = cr.query(builder.build(), INSTANCE_PROJECTION, selection, null, null);
+            while (cur.moveToNext()) {
+
+                EventData event = new EventData();
+                event.Title = cur.getString(INSTANCE_PROJECTION_TITLE_INDEX);
+                event.Start = cur.getLong(INSTANCE_PROJECTION_BEGIN_INDEX);
+                event.End = cur.getLong(INSTANCE_PROJECTION_END_INDEX);
+                event.AllDay = cur.getInt(INSTANCE_PROJECTION_ALL_DAY_INDEX) > 0;
+                event.StartDay = cur.getLong(INSTANCE_PROJECTION_START_DAY_INDEX);
+                event.EndDay = cur.getLong(INSTANCE_PROJECTION_END_DAY_INDEX);
+                event.Color = cur.getInt(INSTANCE_PROJECTION_DISPLAY_COLOR_INDEX);
+                events.add(event);
+            }
+            cur.close();
+
+            RemoteViews widgetView = new RemoteViews(context.getPackageName(), R.layout.calendar_appwidget);
+            RemoteViews cellTitleMaster = new RemoteViews(context.getPackageName(), R.layout.calendar_cell_title);
+            RemoteViews cellRowMaster = new RemoteViews(context.getPackageName(), R.layout.calendar_cell_row);
 
             int[] cellIds = new int[]{R.id.cell_day_1, R.id.cell_day_2, R.id.cell_day_3, R.id.cell_day_4, R.id.cell_day_5, R.id.cell_day_6};
 
-            for (int j = 0; j < cellIds.length; j++) {
+            for (int j = 0; j < cellIds.length; j++, cal.add(Calendar.DAY_OF_WEEK, 1)) {
                 int cellId = cellIds[j];
 
-                cal.add(Calendar.DAY_OF_WEEK, 1);
+                long dayStartMillis = cal.getTimeInMillis();
+                long dayEndMillis = dayStartMillis + (j < 5 ? 1 : 2) * 24 * 3600000;
+
+                long dayStart = Time.getJulianDay(dayStartMillis, cal.getTimeZone().getOffset(cal.getTimeInMillis())/1000);
+                long dayEnd = dayStart + 1;
 
                 // Create an Intent to launch Calendar
-                Uri.Builder builder = CalendarContract.CONTENT_URI.buildUpon();
+                builder = CalendarContract.CONTENT_URI.buildUpon();
                 builder.appendPath("time");
                 ContentUris.appendId(builder, cal.getTimeInMillis());
                 Intent intent = new Intent(Intent.ACTION_VIEW, builder.build());
@@ -50,14 +121,32 @@ public class CalendarAppWidgetProvider extends AppWidgetProvider {
 
                 widgetView.setOnClickPendingIntent(cellId, pendingIntent);
 
-                RemoteViews cellView = cellViewMaster.clone();
-                cellView.setTextViewText(R.id.title, format.format(cal.getTime()));
-                if(today == cal.get(Calendar.DAY_OF_WEEK)) {
-                    cellView.setInt(R.id.title, "setBackgroundResource", android.R.color.holo_orange_dark);
-                }
-
                 widgetView.removeAllViews(cellId);
-                widgetView.addView(cellId, cellView);
+
+                RemoteViews cellTitle = cellTitleMaster.clone();
+                cellTitle.setTextViewText(R.id.title, dateFormat.format(cal.getTime()));
+                if (today == cal.get(Calendar.DAY_OF_WEEK)) {
+                    cellTitle.setInt(R.id.title, "setBackgroundResource", android.R.color.holo_orange_dark);
+                }
+                widgetView.addView(cellId, cellTitle);
+
+                for (int k = 0; k < events.size(); k++) {
+                    EventData event = events.get(k);
+
+                    if(event.AllDay
+                            ? ((event.StartDay >= dayStart && event.StartDay < dayEnd) || (event.EndDay >= dayStart && event.EndDay < dayEnd))
+                            : ((event.Start >= dayStartMillis && event.Start < dayEndMillis) || (event.End >= dayStartMillis && event.End < dayEndMillis))) {
+                        RemoteViews cellRow = cellRowMaster.clone();
+                        if(event.AllDay) {
+                            cellRow.setInt(R.id.title, "setBackgroundColor", event.Color);
+                            cellRow.setTextViewText(R.id.title, event.Title);
+                        } else {
+                            cellRow.setTextViewText(R.id.title, timeFormat.format(new Date(event.Start)) + " " + event.Title);
+                            cellRow.setTextColor(R.id.title, event.Color);
+                        }
+                        widgetView.addView(cellId, cellRow);
+                    }
+                }
 
             }
 
